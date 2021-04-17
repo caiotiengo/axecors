41fea90e3517d0d0e7e928708a5b8c502bd033c7
Caio Tiengo <caiotiengo@gmail.com>
2021-04-17 15:27:33 -0300

    corsaxe

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = ['http:localhost:8100','capacitor://localhost','http://localhost']
+var originWhitelist = ['http://localhost:8100','capacitor://localhost','http://localhost']
 function parseEnvList(env) {
   if (!env) {
     return [];
